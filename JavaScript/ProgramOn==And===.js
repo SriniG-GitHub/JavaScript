d1a3a30cc7f252db164376844ff276d49b34df8f
Srini.G <srini.g.github@gmail.com>
2020-01-05 19:37:41 +0530

    This program is to explain on type and value equality in JS

---
+++ JavaScript/ProgramOn==And===.js
@@ -0,0 +1,18 @@
+var numb1 = 5;  //Number with value as 5
+var string1 ='5'; //String with value as '5'
+
+//Check equal to
+console.log('This is demonstarting on == : ' + (numb1 == string1));  //Returns true as 5 is equal to 5
+
+//Check equal value and equal type
+console.log('This is demonstarting on === : ' + (numb1 === string1)); //Returns false as 5 is Notequal to '5'
+
+
+var numb2 = 10;
+var string2 = '20';
+
+//Check NOT equal to
+console.log('This is demonstarting on != : ' + (numb2 != string2));  //Returns true as 10 is NOT equal to 20
+
+//Check NOT equal value and NOT equal type
+console.log('This is demonstarting on !== : ' + (numb2 !== string2)); //Returns true as 10(Number) is Notequal to '20'(String)
