70afe0bb96042fc8d1dccf3276ec1af6d7fa5fcd
Srini.G <srini.g.github@gmail.com>
2020-01-05 19:22:10 +0530

    This program is to display exponentation in JS using both ways

---
+++ JavaScript/Exponentiation.js
@@ -0,0 +1,11 @@
+var base = 5;
+var pow =5;
+
+//Using Math package and Pow function in it. This is same as in JAVA
+var result1 = Math.pow(base,pow);
+console.log('The power of base raised to pow is : '+ result1);
+
+
+//Using ** operator to find exponentation available in JS.  This is NOT available in JAVA
+var result2= base**pow;
+console.log('The power of base raised to pow is : '+ result2);
